0422f97a2e96c6b40b598c1053de238f20068fc6
JoseMunive <alend.warjerk@gmail.com>
2024-10-22 16:31:14 -0500

    Update WaterLevelController.php

--- app/Http/Controllers/Api/WaterLevelController.php
+++ app/Http/Controllers/Api/WaterLevelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\WaterLevel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class WaterLevelController extends Controller
 {
